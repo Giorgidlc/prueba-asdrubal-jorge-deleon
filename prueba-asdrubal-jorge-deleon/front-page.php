f4ac38ca5bbd356a2dccff571f1414cd0b9e55d4
Giorgi Visual <giorgivisual@gmail.com>
2025-04-05 12:37:01 +0200

    add: content header, footer, front-page, sections

---
+++ prueba-asdrubal-jorge-deleon/front-page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div class="content">
+  <main>
+    <?php if (is_front_page()) include 'sections/section-one.php'; ?>
+  </main>
+  
+  <?php include 'sections/section-two.php'; ?>
+  <?php include 'sections/section-three.php'; ?>
+
+</div>
---
+++ prueba-asdrubal-jorge-deleon/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php blogInfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?> 
+</head>
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+  <header>
+    <h1 id="main-title"><?php the_title() ?></h1>
+    <button id="change-title">👋</button>
+  </header>
+  
+</body>
+</html>
